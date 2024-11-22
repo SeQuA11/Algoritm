63cc4c4182536413c7d9bb55dbb9d4bcd59ee44f
SeQuA11 <151964161+SeQuA11@users.noreply.github.com>
2024-11-22 09:57:43 +0300

    13

--- Lab2.2/src/main/java/wew.java
+++ Lab2.2/src/main/java/wew.java
@@ -7,9 +7,27 @@ public class wew {
         int progress = 1;
         while (progress != 0) {
             System.out.println();
-            double cases = in.nextDouble();
-            switch (progress) {
+            int cases = in.nextInt();
+            switch (cases) {
                 case 1:
+                    System.out.println();
+                    double sum1 = in.nextDouble(), sum2 = in.nextDouble();
+                    System.out.println(sum(sum1, sum2));
+                    break;
+                case 2:
+                    System.out.println();
+                    double diff1 = in.nextDouble(), diff2 = in.nextDouble();
+                    System.out.println(sum(diff1, diff2));
+                    break;
+                case 3:
+                    System.out.println();
+                    double hypotenuse1 = in.nextDouble(), hypotenuse2 = in.nextDouble();
+                    System.out.println(sum(hypotenuse1, hypotenuse2));
+                    break;
+                case 4:
+                case 5:
+                    progress -= 1;
+                    break;
 
             }
 
