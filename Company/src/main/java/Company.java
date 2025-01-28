03753edeba1b8a466693d2f47bd492dc9f838024
SeQuA11 <151964161+SeQuA11@users.noreply.github.com>
2025-01-29 00:21:09 +0300

    29.01.25-0:20

--- Company/src/main/java/Company.java
+++ Company/src/main/java/Company.java
@@ -6,13 +6,23 @@ class Company {
     public String name; // название
     public int persons; // количество сотрудников
     public int money; // месячный фонд зарплаты
-    public String work;
-//    ArrayList<Company> company = new ArrayList<>();
+    public String work; // вид деятельности
 
-    public Company(String user_command) {
+    /*
 
+     */
+    public Company(String user_command) {}
+
+    /*
+
+     */
+    public String getName() {
+        return name;
     }
 
+    /*
+
+     */
     public Company(String name, String work, int persons, int money) {
         this.name = name;
         this.work = work;
@@ -20,22 +30,40 @@ class Company {
         this.money = money;
     }
 
-//    public void listCompany(String name, String work, int persons, int money) {
-//        company.add(new Company(name, work, persons, money));
-//    }
+    /*
 
+     */
     public void displayCompanyInfo() {
-        System.out.println("Название компании: " + name +
-                ", Отрасль: " + work +
-                ", Количество сотрудников: " + persons + " Money" + money);
+        System.out.println();
+        System.out.println("Название компании: " + name
+                + "\nОтрасль: " + work
+                + "\nКоличество сотрудников: " + persons
+                + "\nЗароботная плата: " + money);
     }
 
+    /*
+
+     */
     public void infoCompany(ArrayList<Company> company) {
         System.out.println("Все компании:");
         for (Company com : company) {
             com.displayCompanyInfo();
         }
 
+    }
+
+    /*
+    
+     */
+    public static void removeCompanyByName(ArrayList<Company> company, String companyName) {
+        for (int i = 0; i < company.size(); i++) {
+            if (company.get(i).getName().equals(companyName)) {
+                company.remove(i);
+                System.out.println("Компания '" + companyName + "' удалена.");
+                return; // Выходим после удаления
+            }
+        }
+        System.out.println("Компания '" + companyName + "' не найдена.");
     }
 
 
--- Company/src/main/java/MasCompany.java
+++ Company/src/main/java/MasCompany.java
@@ -3,22 +3,42 @@ import java.util.ArrayList;
 
 class MasCompany {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        boolean bool = true;
-        var user_command = "";
-        ArrayList<Company> company = new ArrayList<>();
-        Company comp = new Company(user_command);
 
+        Scanner in = new Scanner(System.in); //
+        boolean bool = true; //
+        var user_command = ""; //
+        ArrayList<Company> company = new ArrayList<>(); //
+        Company comp = new Company(user_command); //
+
+        System.out.println("Список команд: ");
+        System.out.println();
+        System.out.println("////////////////////////////////////////////////////////////");
+        System.out.println("1. Help - меню помощи.");
+        System.out.println("2. Info - информация о компаниях.");
+        System.out.println("3. Add - добавление данных о компании.");
+        System.out.println("4. Remove - удаление данных о компании.");
+        System.out.println("5. Exit - выход из программы.");
+        System.out.println("////////////////////////////////////////////////////////////");
+        System.out.println();
+
+        /*
+
+         */
         company.add(new Company("RotFish","Туризм", 115, 34000));
         company.add(new Company("Genesis","Геодезия", 1567, 68000));
         company.add(new Company("ITHome","Серверная", 789, 115000));
         company.add(new Company("CoffeTime","Кофейня", 24, 53000));
+        //
         comp.infoCompany(company);
 
+        /*
+
+         */
         while (bool) {
             // Приглашение ввести команду
-            System.out.println("Введите команду: ");
-            user_command = in.nextLine();
+            System.out.println();
+            System.out.print("Введите команду: ");
+            user_command = in.nextLine(); //
 
             // Обработка команды с помощью оператора ветвления
             switch (user_command) {
@@ -26,41 +46,53 @@ class MasCompany {
                 case "1": {
                     System.out.println("Список команд: ");
                     System.out.println("////////////////////////////////////////////////////////////");
-                    System.out.println("create_pers: команда для создания персонажа.");
-                    System.out.println("remove: команда для изменения одной из характеристик.");
-                    System.out.println("Har_pers: перечень характеристик персонажа");
-                    System.out.println("exit: команда для выхода.");
+                    System.out.println("1. Help - меню помощи.");
+                    System.out.println("2. Info - информация о компаниях.");
+                    System.out.println("3. Add - добавление данных о компании.");
+                    System.out.println("4. Remove - удаление данных о компании.");
+                    System.out.println("5. Exit - выход из программы.");
                     System.out.println("////////////////////////////////////////////////////////////");
                     break;
                 }
-
+                //
                 case "2": {
+                    //
                     comp.infoCompany(company);
                     break;
                 }
-
+                //
                 case "3": {
-                    
-                    company.add(new Company("CoffeTime","Кофейня", 24, 53000));
+                    System.out.print("Введите название компании: ");
+                    String name = in.nextLine();
+                    System.out.print("Введите деятельность компании: ");
+                    String work = in.nextLine();
+                    System.out.print("Введите количество сотрудников в компании: ");
+                    int persons = Integer.parseInt(in.nextLine());
+                    System.out.print("Введите размер з/п компании: ");
+                    int money = Integer.parseInt(in.nextLine());
+                    //
+                    company.add(new Company(name,work, persons, money));
                     break;
                 }
-
+                //
                 case "4": {
-
+                    System.out.print("Введите название компании для удаления: ");
+                    String name = in.nextLine();
+                    //
+                    Company.removeCompanyByName(company, name);
+                    break;
                 }
-
-
+                //
                 case "5": {
                     bool = false;
                     // Цикл завершается, и программа завершит своё выполнение
                     break;
                 }
-
                 // Если команду не удалось определить
                 default: {
                     System.out.println("Ваша команда не определена, пожалуйста, повторите снова");
-                    System.out.println("Для вывода списка команд введите команду help");
-                    System.out.println("Для завершения программы введите команду exit");
+                    System.out.println("Для вывода списка команд введите команду 1");
+                    System.out.println("Для завершения программы введите команду 5");
                     break;
                 }
             }
