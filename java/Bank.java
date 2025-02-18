9382050a35006b851fef2f949ac277200587007b
SeQuA11 <151964161+SeQuA11@users.noreply.github.com>
2025-02-19 00:21:26 +0300

    ол

---
+++ java/Bank.java
@@ -0,0 +1,16 @@
+public class Bank {
+    static String Family;
+    static int passport;
+    static int quantityScore;
+    static int numberScore;
+
+    public Bank(String appeal) {}
+
+//    public void displayCompanyInfo() {
+//        System.out.println();
+//        System.out.println("Название компании: " + name
+//                + "\nОтрасль: " + work
+//                + "\nКоличество сотрудников: " + persons
+//                + "\nЗароботная плата: " + money);
+//    }
+}
