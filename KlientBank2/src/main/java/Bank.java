8e54408becf01ce2dfde5196d59f675c81fedba3
SeQuA11 <151964161+SeQuA11@users.noreply.github.com>
2025-02-19 09:54:15 +0300

    uyfu

---
+++ KlientBank2/src/main/java/Bank.java
@@ -0,0 +1,19 @@
+public class Bank {
+    static String FIO;
+    static int passport;
+
+    public Bank(String appeal) {}
+
+    public Bank(String FIO, int passport) {
+        this.FIO = FIO;
+        this.passport = passport;
+    }
+
+//    public void displayCompanyInfo() {
+//        System.out.println();
+//        System.out.println("Название компании: " + name
+//                + "\nОтрасль: " + work
+//                + "\nКоличество сотрудников: " + persons
+//                + "\nЗароботная плата: " + money);
+//    }
+}
---
+++ KlientBank2/src/main/java/Score.java
@@ -0,0 +1,11 @@
+public class Score {
+    static int quantityScore;
+    static int numberScore;
+    static int sizeContribution;
+
+    public Score(int quantityScore, int numberScore, int sizeContribution) {
+        this. quantityScore = quantityScore;
+        this.numberScore = numberScore;
+        this.sizeContribution = sizeContribution;
+    }
+}
