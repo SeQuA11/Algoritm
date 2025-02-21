6f5c58932dfc16ab6c9819907d80202aea3e3694
SeQuA11 <151964161+SeQuA11@users.noreply.github.com>
2025-02-21 23:43:09 +0300

    ghu

--- KlientBank2/src/main/java/Bank.java
+++ KlientBank2/src/main/java/Bank.java
@@ -1,38 +1,10 @@
-import java.util.ArrayList;
-import java.util.Random;
-
 public class Bank {
-    Random rnd = new Random();
-    static String FIO;
+    static String Family;
     static int passport;
-    ArrayList<Score> score;
+    static int quantityScore;
+    static int numberScore;
 
     public Bank(String appeal) {}
-
-    public Bank(String FIO, int passport) {
-        this.FIO = FIO;
-        this.passport = passport;
-        this.score = new ArrayList<>();
-    }
-
-    public void addScore() {
-        score.add(new Score());
-    }
-
-    public void infoMedication() {
-        System.out.println("Клиент '" + FIO + "'" + "\nПаспорт" + passport);
-        if (score.isEmpty()){
-            System.out.println("");
-            return;
-        }
-
-        // Перебираем и выводим информацию о каждом медикаменте
-        for (Score sc : score) {
-            for (int i = 1; i < score.size(); i++) {
-                System.out.println(i + ": "+ sc);
-            }
-        }
-    }
 
 //    public void displayCompanyInfo() {
 //        System.out.println();
