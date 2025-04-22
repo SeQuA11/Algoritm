889777b485475e4b115809a27fe46ef199ad8c28
SeQuA11 <151964161+SeQuA11@users.noreply.github.com>
2025-04-22 23:31:56 +0300

    мы

---
+++ 3. Основы ООП/TDD/Apteca/src/test/java/AptekTestTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AptekTestTest {
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_1_2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_1_3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_2_1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_2_2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_3_1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_3_2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddMedicine_Apteka_3_3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFullInfo_Apteka_1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFullInfo_Apteka_4_witchNullMedicine() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMostExpensiveMedicinePrice() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTotalCost() {
+    }
+}
