969cf4a1fb4623e634900fc6ad50bba565dc75c1
SeQuA11 <151964161+SeQuA11@users.noreply.github.com>
2025-09-09 22:35:29 +0300

    гна

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++;
+        System.out.println(a + "," + b);
+    }
+}
